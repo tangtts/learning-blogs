d3ab534a5de66cb931d73034783759b0ac3557b6
tangshuokun <2939117014@qq.com>
2025-02-15 10:11:48 +0800

    流水线

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f https://github.com/tangtts/learning-blogs.git master
+git push -f https://github.com/tangtts/learning-blogs.git main
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
